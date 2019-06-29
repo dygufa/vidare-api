eb1443f64278628c3e6d18a96049511a77211f5b
Rodrigo Araujo <dygufa@gmail.com>
2019-06-29 19:50:22 -0300

    feat: implement login endpoint

---
+++ src/controllers/users.ts
@@ -0,0 +1,38 @@
+require('dotenv').config();
+import { Request, Response } from "express";
+import { UserModel } from "../models";
+import { expressError } from "../utils";
+import * as jwt from "jsonwebtoken";
+
+export const login = async (req: Request, res: Response) => {
+    const { googleId, name, email, avatarUrl } = req.body;
+    if (!googleId) expressError(res, "googleId is not defined");
+
+    let user = await UserModel.findOne({ googleId });
+
+    if (!user) {
+        user = await UserModel.create({
+            name,
+            email,
+            bloodType: null,
+            level: 1,
+            googleId,
+            avatarUrl,
+            createdAt: new Date()
+        });
+    }
+
+    const jwtToken = await jwt.sign({
+        user: {
+            id: user.id,
+        }
+    }, process.env.JWT_SECRET!);
+
+    res.json({
+        ok: true,
+        data: {
+            user,
+            token: jwtToken
+        }
+    });
+}
