7a0d5b003b3d097b299453778904d5268401e0b6
Rodrigo Araujo <dygufa@gmail.com>
2019-06-29 19:51:16 -0300

    chore: add new controllers and models to index files

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,8 @@
 import * as ProductController from "./products";
+import * as VoucherController from "./vouchers";
+import * as UserController from "./users";
+import * as BloodDonationController from "./bloodDonations";
 
-export { ProductController };
+export {
+    ProductController, VoucherController, UserController, BloodDonationController
+};
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,6 @@
 import ProductModel from './products';
 import UserModel from './users';
+import VoucherModel from './vouchers';
+import BloodDonationModel from "./bloodDonations";
 
-export { ProductModel, UserModel };
+export { ProductModel, UserModel, VoucherModel, BloodDonationModel };
